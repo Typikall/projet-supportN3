1c9d5ffd1749f0e9d3537f4dba9ec14b472d6306
PR271F5N <PR271F5N@atlas.edf.fr>
2016-03-17 17:43:05 +0100

    .

--- webprojet/webapp/server/server.js
+++
@@ -1,7 +0,0 @@
-var http= require('http');
-http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Bonjour la Terre');
-}).listen(80);
-
-console.log('Server running at http://127.0.0.1:80/');
