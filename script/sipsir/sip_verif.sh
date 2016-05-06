7fe5314ce2f75b7cdeb4506a905a51b2d8335279
Typikall <PR271F5N@atlas.edf.fr>
2016-05-06 10:46:28 +0200

    .

---
+++ script/sipsir/sip_verif.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+neutre='\e[0;m'
+rouge='\e[0;31m'
+for var in $(grep -v "#" $HOME/prakotoarisoa/sipsir/list_sipsir|cut -d"|" -f2)
+do
+echo -e $neutre"Ping de $var"
+ping  $var 1>/dev/null
+if [ $? -ne 0 ]
+then
+echo -e $rouge"KO"
+else
+echo -e "OK"
+fi
+echo -e $neutre"--------"
+done  
